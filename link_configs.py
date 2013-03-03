bab22fa1a3337bcf52b8a8dec771785acbe73467
Adam Rothman <rothman.adam@gmail.com>
2013-03-03 11:21:40 -0800

    Refactor - simplify

--- link_configs.py
+++ link_configs.py
@@ -5,12 +5,15 @@ from os import listdir, remove, symlink
 from os.path import basename, expanduser, isdir, isfile, islink, join, lexists
 from shutil import rmtree
 
-config_root = join(expanduser('~'), 'Dropbox', 'Config')
-skip_dirs = set(['alfred', 'oh-my-zsh'])
+user_home = expanduser('~')
+config_root = join(user_home, 'Dropbox', 'Config')
 
 parser = ArgumentParser(description='Symlink config files into system locations.')
 parser.add_argument('-o', '--overwrite', help='overwrite existing files when linking', action='store_true')
 args = parser.parse_args()
+
+def listdir_visible(path):
+  return [f for f in listdir(path) if f[0] != '.']
 
 def link(source, target):
   if lexists(target):
@@ -30,20 +33,24 @@ def link(source, target):
   symlink(source, target)
   print('Created link\t{} -> {}'.format(target, source))
 
+def link_dotfiles():
+  source = join(config_root, 'dotfiles')
+  for f in listdir_visible(source):
+    link(join(source, f), join(user_home, '.' + f))
+
+def link_preferences():
+  source = join(config_root, 'preferences')
+  for f in listdir_visible(source):
+    link(join(source, f), join(user_home, 'Library', 'Preferences', f))
+
+def link_support():
+  source = join(config_root, 'support')
+  for d in listdir_visible(source):
+    d_path = join(source, d)
+    for f in listdir_visible(d_path):
+      link(join(d_path, f), join(user_home, 'Library', 'Application Support', d, f))
+
 if __name__ == '__main__':
-  for d in listdir(config_root):
-    d_path = join(config_root, d)
-    if d[0] == '.' or not isdir(d_path) or d in skip_dirs:
-      continue
-    for f in listdir(d_path):
-      f_path = join(d_path, f)
-      if f[0] == '.':
-        continue
-      target = expanduser('~')
-      if d == 'dotfiles':
-        target = join(target, '.' + f)
-      elif d == 'sublime':
-        target = join(target, 'Library', 'Application Support', 'Sublime Text 2', f)
-      elif d == 'preferences':
-        target = join(target, 'Library', 'Preferences', f)
-      link(f_path, target)
+  link_dotfiles()
+  link_preferences()
+  link_support()
