3b319dd926a1bb26e91119ec2c5a0d9bb5da6f57
Adam Rothman <rothman.adam@gmail.com>
2014-06-03 21:40:11 -0700

    Update backup.sh with compression and a helpful comment

--- backup.sh
+++ backup.sh
@@ -2,16 +2,23 @@
 
 # Based on https://github.com/necolas/dotfiles/blob/master/bin/backup
 
+# The --rsync-path argument is specific to my setup and my server, you
+# will probably want to remove it if you aren't copying from a remote
+# machine and/or that's not where rsync lives on said machine.
+
 rsync --archive \
+      --compress \
       --delete \
       --delete-excluded \
       --exclude-from="$BACKUP_EXCLUDE" \
       --hard-links \
       --one-file-system \
       --progress \
+      --rsync-path=/opt/bin/rsync \
       --sparse \
       --verbose \
       --xattrs \
-      "$BACKUP_SOURCE" "$BACKUP_DESTINATION"
+      "$BACKUP_SOURCE" \
+      "$BACKUP_DESTINATION"
 
 exit 0
